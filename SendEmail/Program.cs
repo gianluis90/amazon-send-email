201b419ce64728721f3f1e585fe5c87b3784426c
gianluis90 <gianluis90@hotmail.com>
2017-06-22 16:40:28 -0400

    Add project files.

---
+++ SendEmail/Program.cs
@@ -0,0 +1,29 @@
+﻿using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System.IO;
+
+namespace SendEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Remember to enter your (AWSAccessKeyID, AWSSecretAccessKey) if not using and IAM User with credentials assigned to your instance and your RegionEndpoint
+            using (var client = new AmazonSimpleEmailServiceClient("YourAWSAccessKeyID", "YourAWSSecretAccessKey", RegionEndpoint.USEast1))
+            {
+                var emailRequest =  new SendEmailRequest()
+                {
+                    Source = "FROMADDRESS@TEST.COM",
+                    Destination = new Destination(),
+                    Message = new Message()
+                };
+
+                emailRequest.Destination.ToAddresses.Add("TOADDRESS@TEST.COM");
+                emailRequest.Message.Subject = new Content("Hello World");
+                emailRequest.Message.Body = new Body(new Content("Hello World"));
+                client.SendEmail(emailRequest);
+            }
+        }
+    }
+}
